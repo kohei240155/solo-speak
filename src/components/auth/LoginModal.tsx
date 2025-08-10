c195af30c8a154e0ee14045f1ea3dc5bd1f1c16e
kohei240155 <k.pino519@gmail.com>
2025-08-10 09:17:31 -0700

    s

--- src/components/auth/LoginModal.tsx
+++ src/components/auth/LoginModal.tsx
@@ -2,7 +2,6 @@
 
 import { useState, useEffect, useRef } from 'react'
 import Image from 'next/image'
-import { GoAlert } from 'react-icons/go'
 import { useAuth } from '@/contexts/AuthContext'
 import BaseModal from '../common/BaseModal'
 
