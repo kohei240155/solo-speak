301463afd0258a5a8f1106beff07f2876ca08d67
kohei240155 <k.pino519@gmail.com>
2025-08-31 10:43:36 -0700

    s

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ import CTASection from '@/components/layout/CTASection'
 import { useState, useEffect } from 'react'
 
 export default function Home() {
-  const { t, isLoading: isLoadingTranslation } = useTranslation('common')
+  const { isLoading: isLoadingTranslation } = useTranslation('common')
   const { showLoginModal } = useAuth()
   const visibleSections = useScrollAnimation()
   const [showSplash, setShowSplash] = useState(true)
